a35462cc96d192f8480584d69a31a80d359dead3
Irving Valencia <75141924+irving-vale@users.noreply.github.com>
2024-08-07 14:50:50 -0600

    feat: se implemento un backend rapido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {SharedModule} from "./shared/shared.module";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {provideHttpClient} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -15,10 +16,12 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Hero} from "../../interfaces/hero.interface";
+import {HeroesService} from "../../services/heroes.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
   styles: ``
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit, OnDestroy {
+
+  constructor(private heroesService: HeroesService) {
+
+
+  }
+
+  public heroes: Hero[] = [];
+  private subscription: Subscription | undefined;
+
+  ngOnInit(): void {
+    this.subscription = this.heroesService.getHeroes().subscribe(heroes => this.heroes = heroes);
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 
 }
